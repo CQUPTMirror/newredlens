a95b8c47d211328e07dd1b10369de0e8cfd30f71
qwqVictor <i@qwq.ren>
2023-09-25 13:24:05 +0800

    fix(types): use second-level timestamp

--- src/utils/shims.ts
+++ src/utils/shims.ts
@@ -3,14 +3,18 @@ import utils from './index'
 
 const dateFormat = utils.dateFormat
 
+const dateFromMirrorItem = (d: number): Date => {
+  return new Date(d * 1000)
+}
+
 const mirrorViewShim = (mirrorItem: MirrorItem): MirrorViewItem => {
   return { ...mirrorItem,
-    lastUpdateTimeString: mirrorItem.lastUpdate !== undefined ? dateFormat(new Date(mirrorItem.lastUpdate)) : undefined,
-    lastStartedTimeString: mirrorItem.lastStarted !== undefined ? dateFormat(new Date(mirrorItem.lastStarted)) : undefined,
-    lastEndedTimeString: mirrorItem.lastEnded !== undefined ? dateFormat(new Date(mirrorItem.lastEnded)) : undefined,
-    nextScheduleTimeString: mirrorItem.nextSchedule !== undefined ? dateFormat(new Date(mirrorItem.nextSchedule)) : undefined,
-    lastOnlineTimeString: mirrorItem.lastOnline !== undefined ? dateFormat(new Date(mirrorItem.lastOnline)) : undefined,
-    lastRegisterTimeString: mirrorItem.lastRegister !== undefined ? dateFormat(new Date(mirrorItem.lastRegister)) : undefined
+    lastUpdateTimeString: mirrorItem.lastUpdate !== undefined ? dateFormat(dateFromMirrorItem(mirrorItem.lastUpdate)) : undefined,
+    lastStartedTimeString: mirrorItem.lastStarted !== undefined ? dateFormat(dateFromMirrorItem(mirrorItem.lastStarted)) : undefined,
+    lastEndedTimeString: mirrorItem.lastEnded !== undefined ? dateFormat(dateFromMirrorItem(mirrorItem.lastEnded)) : undefined,
+    nextScheduleTimeString: mirrorItem.nextSchedule !== undefined ? dateFormat(dateFromMirrorItem(mirrorItem.nextSchedule)) : undefined,
+    lastOnlineTimeString: mirrorItem.lastOnline !== undefined ? dateFormat(dateFromMirrorItem(mirrorItem.lastOnline)) : undefined,
+    lastRegisterTimeString: mirrorItem.lastRegister !== undefined ? dateFormat(dateFromMirrorItem(mirrorItem.lastRegister)) : undefined
   }
 }
 
